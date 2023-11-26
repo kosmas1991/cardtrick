15325b837d8895e81e9b579627c298678e165bdf
kosmas1991 <kosmasgou@gmail.com>
2023-11-26 12:27:01 +0200

    a

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,28 +1,11 @@
-import 'dart:convert';
-
 import 'package:cardtrick/cubits/instruction/instruction_cubit.dart';
-import 'package:cardtrick/models/21pickedcards.dart';
-import 'package:cardtrick/models/newdeck.dart';
 import 'package:cardtrick/widgets/showbutton.dart';
 import 'package:cardtrick/widgets/showcards.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late Future<PickedCards21> pickedCards21;
-  @override
-  void initState() {
-    pickedCards21 = fetchNewDeck();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,21 +32,8 @@ class _HomePageState extends State<HomePage> {
                 constraints: BoxConstraints.expand(),
                 child: Column(
                   children: [
-                    Flexible(
-                        flex: 9,
-                        child: FutureBuilder(
-                          future: pickedCards21,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              return ShowCards(
-                                cards: snapshot.data!.cards!,
-                              );
-                            } else {
-                              return CircularProgressIndicator();
-                            }
-                          },
-                        )),
-                    Flexible(flex: 1, child: ShowButton()),
+                    Flexible(flex: 92, child: ShowCards()),
+                    Flexible(flex: 8, child: ShowButton()),
                   ],
                 ))),
         Flexible(
@@ -79,29 +49,5 @@ class _HomePageState extends State<HomePage> {
                 )))),
       ],
     )));
-  }
-
-  Future<PickedCards21> fetchNewDeck() async {
-    final newDeck;
-    final response = await http.get(Uri.parse(
-        'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'));
-
-    if (response.statusCode == 200) {
-      newDeck =
-          NewDeck.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('Failed');
-    }
-    final response2 = await http.get(Uri.parse(
-        'https://deckofcardsapi.com/api/deck/${newDeck.deckId}/draw/?count=21'));
-    print('New deck id: ${newDeck.deckId} ');
-
-    if (response2.statusCode == 200) {
-      print(response2.body);
-      return PickedCards21.fromJson(
-          jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('Failed');
-    }
   }
 }
--- lib/widgets/showcards.dart
+++ lib/widgets/showcards.dart
@@ -1,10 +1,8 @@
-import 'package:cardtrick/models/21pickedcards.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class ShowCards extends StatelessWidget {
-  final List<Cards> cards;
-  const ShowCards({required this.cards, super.key});
+  const ShowCards({super.key});
 
   @override
   Widget build(BuildContext context) {
